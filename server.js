c8a06e349e77e4f42eaa36320219fc3fa990b7b8
Bethanie <bethanielys@gmail.com>
2024-01-23 01:13:59 +0800

    1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 
 app.use("/users", require("./controllers/users")); //sets up routing for user-related requests in your Express.js application.
 app.use("/products", require("./controllers/products")); //sets up routing for product-related requests in your Express.js application.
-app.use("/carts", require("./controllers/carts")); //sets up routing for product-related requests in your Express.js application.
+app.use("/cart", require("./controllers/carts")); //sets up routing for product-related requests in your Express.js application.
 app.use("/orders", require("./controllers/orders")); //sets up routing for product-related requests in your Express.js application.
 
 app.listen(PORT, console.log("App is swimming on PORT: " + PORT)); //when you run this server.js file, it will send to the port number
